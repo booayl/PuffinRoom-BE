3c3f5f5793b9c9d26c73fdc84ac28d62abf57788
booayl <angelaboo226@icloud.com>
2024-04-15 16:37:00 +0100

    completed ticket 6 of GET /api/articles/:article_id/comments

--- controllers.js
+++ controllers.js
@@ -1,4 +1,4 @@
-const { fetchTopics, fetchArticleById, fetchArticles } = require("./model");
+const { fetchTopics, fetchArticleById, fetchArticles, fetchCommentsByArticleID } = require("./model");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
@@ -17,10 +17,19 @@ exports.getArticleById = (req, res, next) => {
     });
 };
 
-exports.getArticles = (req,res,next)=>{
-    const {sort_by , order} = req.query
-    fetchArticles(sort_by , order)
-    .then((articles)=>{
-        res.status(200).send({allArticles : articles})
-    })
-}
+exports.getArticles = (req, res, next) => {
+  const { sort_by, order } = req.query;
+  fetchArticles(sort_by, order).then((articles) => {
+    res.status(200).send({ allArticles: articles });
+  });
+};
+
+exports.getCommentsByArticleID = (req, res, next) => {
+  const { article_id } = req.params;
+  const { sort_by, order } = req.query;
+  fetchCommentsByArticleID(article_id, sort_by, order).then((comments) => {
+    res.status(200).send({ allComments: comments });
+  }).catch((err) => {
+    next(err);
+  });
+};
--- model.js
+++ model.js
@@ -17,19 +17,34 @@ exports.fetchArticleById = (article_id) => {
     });
 };
 
-exports.fetchArticles = (sort_by="created_at",order="DESC") =>{
-    let sqlQueryString = `SELECT articles.*,
+exports.fetchArticles = (sort_by = "created_at", order = "DESC") => {
+  let sqlQueryString = `SELECT articles.*,
     COUNT(comments.article_id)::INT AS comment_count
     FROM comments
     RIGHT JOIN articles
     ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id `
+    GROUP BY articles.article_id `;
 
-    sqlQueryString += `ORDER BY ${sort_by} ${order}`
+  sqlQueryString += `ORDER BY ${sort_by} ${order}`;
 
-    return db
-    .query(sqlQueryString).then(({rows})=>{
+  return db.query(sqlQueryString).then(({ rows }) => {
+    return rows;
+  });
+};
+
+exports.fetchCommentsByArticleID = (article_id,sort_by = "created_at", order = "DESC") =>{
+    let sqlQueryString = `SELECT comments.* 
+    FROM comments 
+    INNER JOIN articles 
+    ON comments.article_id = articles.article_id `
+
+    sqlQueryString += `WHERE comments.article_id = $1 ORDER BY ${sort_by} ${order};`
+
+    return db.query(sqlQueryString,[article_id])
+    .then(({rows})=>{
+        if (rows.length === 0) {
+            return Promise.reject({ status: 400, msg: "Invalid ID Input" });
+          }
         return rows
     })
-
 }
