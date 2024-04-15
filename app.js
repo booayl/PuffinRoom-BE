91171b2d1bbc059ffdc30ab8091f1dc27c8aa6f3
booayl <angelaboo226@icloud.com>
2024-04-15 15:30:45 +0100

    corrected some typos

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getArticles)
 
 //Error Handling Middleware
-app.use((req, res) => {
+app.all("*",(req, res) => {
   res.status(404).send("Invalid Endpoint");
 });
 
