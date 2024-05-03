0fe4b2dabf7b147c2fb4fe49c90393bc8fe6b357
booayl <angelaboo226@icloud.com>
2024-05-03 16:38:24 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 //Respond a list of all available endpoints from endpoint.json
 app.use("/api", apiRouter);
